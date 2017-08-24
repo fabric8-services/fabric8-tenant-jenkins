64b95607036849059954fa553bab910901581bc9
rawlingsj <rawlingsj80@gmail.com>
2017-08-24 14:59:34 +0100

    consolodate generated YAML into specific tenant repos

---
+++ Jenkinsfile
@@ -0,0 +1,35 @@
+#!/usr/bin/groovy
+@Library('github.com/fabric8io/fabric8-pipeline-library@master')
+def utils = new io.fabric8.Utils()
+releaseNode {
+  try {
+    checkout scm
+    readTrusted 'release.groovy'
+
+    if (utils.isCI()) {
+
+      mavenCI{}
+
+    } else if (utils.isCD()) {
+      sh "git remote set-url origin git@github.com:fabric8io/fabric8-tenant-jenkins.git"
+
+      def pipeline = load 'release.groovy'
+      def stagedProject
+
+      stage('Stage') {
+        stagedProject = pipeline.stage()
+      }
+
+      stage('Promote') {
+        pipeline.release(stagedProject)
+      }
+
+      stage ('Update downstream dependencies'){
+        pipeline.updatefabric8Tenant(stagedProject)
+      }
+    }
+  } catch (err) {
+    hubot room: 'release', message: "${env.JOB_NAME} failed: ${err}"
+    error "${err}"
+  }
+}
---
+++ release.groovy
@@ -0,0 +1,61 @@
+#!/usr/bin/groovy
+def stage(){
+  return stageProject{
+    project = 'fabric8io/fabric8-tenant-jenkins'
+    useGitTagForNextVersion = true
+  }
+}
+
+def approveRelease(project){
+  def releaseVersion = project[1]
+  approve{
+    room = null
+    version = releaseVersion
+    console = null
+    environment = 'fabric8'
+  }
+}
+
+def release(project){
+  releaseProject{
+    stagedProject = project
+    useGitTagForNextVersion = true
+    helmPush = false
+    groupId = 'io.fabric8.tenant'
+    githubOrganisation = 'fabric8io'
+    artifactIdToWatchInCentral = 'tenant-jenkins'
+    artifactExtensionToWatchInCentral = 'pom'
+    promoteToDockerRegistry = 'docker.io'
+    dockerOrganisation = 'fabric8'
+    imagesToPromoteToDockerHub = []
+    extraImagesToTag = null
+  }
+}
+
+def updatefabric8Tenant(releaseVersion){
+  ws{
+    container(name: 'clients') {
+      def flow = new io.fabric8.Fabric8Commands()
+      sh 'chmod 600 /root/.ssh-git/ssh-key'
+      sh 'chmod 600 /root/.ssh-git/ssh-key.pub'
+      sh 'chmod 700 /root/.ssh-git'
+
+      git 'git@github.com:fabric8-services/fabric8-tenant.git'
+
+      sh "git config user.email fabric8cd@gmail.com"
+      sh "git config user.name fabric8-cd"
+
+      def uid = UUID.randomUUID().toString()
+      sh "git checkout -b versionUpdate${uid}"
+
+      sh "echo ${releaseVersion} > JENKINS_VERSION"
+      def message = "Update fabric8-online version to ${releaseVersion}"
+      sh "git commit -a -m \"${message}\""
+      sh "git push origin versionUpdate${uid}"
+
+      def prId = flow.createPullRequest(message,'fabric8-services/fabric8-tenant',"versionUpdate${uid}")
+      flow.mergePR('fabric8-services/fabric8-tenant',prId)
+    }
+  }
+}
+return this;
